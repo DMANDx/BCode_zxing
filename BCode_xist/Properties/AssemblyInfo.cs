3c511200cda4c6bc3632ba2e772283830f8351f5
DMAND <dmandxist@gmail.com>
2023-03-08 10:29:32 +0300

    Create AssemblyInfo.cs

---
+++ BCode_xist/Properties/AssemblyInfo.cs
@@ -0,0 +1,51 @@
+﻿//INSTANT C# NOTE: Formerly VB project-level imports:
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Linq;
+using System.Xml.Linq;
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+// Review the values of the assembly attributes
+
+[assembly: AssemblyTitle("BCode_xist")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("post")]
+[assembly: AssemblyProduct("BCode_xist")]
+[assembly: AssemblyCopyright("Copyright © post 2018")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+
+//The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("94a5a541-4f4c-48a0-8f59-ef5374127132")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// <Assembly: AssemblyVersion("1.0.*")> 
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
